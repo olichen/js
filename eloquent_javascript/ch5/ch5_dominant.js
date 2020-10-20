ea1b4a5afdea8f114ab1cff904de11bc9319603f
Oliver Chen <olichen@ucdavis.edu>
2020-10-20 03:56:27 -0700

    ch5

---
+++ eloquent_javascript/ch5/ch5_dominant.js
@@ -0,0 +1,43 @@
+require('./scripts.js');
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function dominantDirection(str) {
+  let count = { 'rtl': 0, 'ltr': 0 };
+  for (let c of str) {
+    let script = characterScript(c.codePointAt(0));
+    if (script)
+      count[script.direction] += 1;
+  }
+  if (count['rtl'] > count['ltr'])
+    return 'rtl';
+  return 'ltr';
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
